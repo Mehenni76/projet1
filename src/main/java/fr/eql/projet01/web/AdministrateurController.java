e7238986b9426481455507bf6e5003505dacbb99
mehenni76 <djaddamehenni@gmail.comclear>
2021-08-02 18:38:15 +0200

    message

--- src/main/java/fr/eql/projet01/web/AdministrateurController.java
+++ src/main/java/fr/eql/projet01/web/AdministrateurController.java
@@ -33,7 +33,7 @@ import fr.eql.projet01.service.UtilisateurService;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping(value = "/administrateur")
+@RequestMapping(value="/administrateur", headers="Accept=application/json")
 public class AdministrateurController {
 
 	@Autowired
