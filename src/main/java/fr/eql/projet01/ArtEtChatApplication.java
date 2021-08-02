efa85750022479bc96ff22fcba37d6bc70aeb733
mehenni76 <djaddamehenni@gmail.comclear>
2021-08-02 14:39:06 +0200

    message

--- src/main/java/fr/eql/projet01/ArtEtChatApplication.java
+++ src/main/java/fr/eql/projet01/ArtEtChatApplication.java
@@ -3,8 +3,8 @@ package fr.eql.projet01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/fr/eql/projet01/springSecurity/WebSecurityConfig.java
+++ src/main/java/fr/eql/projet01/springSecurity/WebSecurityConfig.java
@@ -73,5 +73,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	      source.registerCorsConfiguration("/**", config);
 	      return source;
 	    }
-	 }
+	 
 }
